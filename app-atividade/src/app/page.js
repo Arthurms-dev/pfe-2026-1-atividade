f01872e35658e587992ea253e898efcc5fa4782a
Arthurms-dev <arthurms707@gmail.com>
2026-02-13 01:58:47 +0000

    Atividade de presença - Programação front end

---
+++ app-atividade/src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1>Pessoas boas moldam mais pessoas boas. Porém, o mundo não possui tantas.</h1>
+    </div>
+  );
+}
